203c37ada7bfc76f5ebc72bc8c66f94ab9075412
Gitthuma <george.githuma@gmail.com>
2021-11-01 21:27:50 +0300

    Use an if statement to check if number is even a print the result

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 # get an input of type int from the user
 num = int(input("Please enter a number: "))
+
+# Use an if statement to check if number is even a print the result
+if num % 2 == 0:
+    print("That is an even number")
+else:
+    print("That is an odd number")
